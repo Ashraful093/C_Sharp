f5c70f7f37ce390233396360254c9b9e4e664c74
Ashraful Islam <102016365+Ashraful093@users.noreply.github.com>
2022-06-01 00:36:46 +0600

    Add files via upload

---
+++ showarray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace showellement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ sum array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayprob1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraydata = { 3, 4, 5, 7, 6, 5, 4, 3, 5, 6, 7, 7, 8, 9, 7, 6, 5 };
+            Console.WriteLine("\narray 1:[{0}]", string.Join(",", arraydata));
+            var sum = 0;
+            for (var k=0; k<arraydata.Length;k++)
+            {
+                sum += arraydata[k];
+            }
+            Console.WriteLine("The sum of the ellements is " + sum);
+            Console.ReadLine();
+        }
+    }
+}
