2f05193a3c80edd1198a8a5de10fc8817c809eed
Ashraful Islam <102016365+Ashraful093@users.noreply.github.com>
2022-06-02 20:39:55 +0600

    Add files via upload

---
+++ reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reversearray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n , reverse =0, rem;
+            Console.WriteLine(" Enter a number:");
+            n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+
+            }
+            Console.WriteLine("Reverse Number: " + reverse);
+            Console.WriteLine(" ");
+            Console.ReadLine();
+
+        }
+    }
+}
