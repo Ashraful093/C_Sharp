587326fb1204db20436ccb3fe98bd12f516f1fc1
Ashraful Islam <102016365+Ashraful093@users.noreply.github.com>
2022-06-03 19:46:56 +0600

    Add files via upload

---
+++ Guess the game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "Ashraful";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && ! outOfGuesses) 
+            {
+                if (guessCount< guessLimit)
+                    {
+                    Console.WriteLine("Enter Guess");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+               
+            }
+            if(outOfGuesses)
+            {
+                Console.WriteLine("You Lose!");
+            }
+            else
+            {
+                Console.WriteLine("You Win!");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab task 2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myproggramme1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter two numbers to add substract multiple and devide");
+            int number1, number2 = 0;
+            int  add, substract, multiple, devide = 0;
+            Console.WriteLine("please enter first numvber ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter socend numvber ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            add = number1 + number2;
+            substract = number1 - number2;
+            multiple = number1 * number2;
+            devide = number1 / number2;
+
+            Console.WriteLine("Results");
+            Console.WriteLine("Addition{0}", add);
+            Console.WriteLine("Subtraction{0}", substract);
+            Console.WriteLine("Multipication{0}", multiple);
+            Console.WriteLine("Devide{0}",devide);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
