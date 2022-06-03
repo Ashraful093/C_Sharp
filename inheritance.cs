8cc4722203f7fd8f3595dc3050e194e9224eb331
Ashraful Islam <102016365+Ashraful093@users.noreply.github.com>
2022-06-03 19:45:39 +0600

    Add files via upload

---
+++ inheritance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritancetask1
+{
+    public class Primary_School
+    {
+        public string name;
+    }
+    public class Midium_School : Primary_School
+    {
+        public class Midium()
+        {
+            
+        }
+    } 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
