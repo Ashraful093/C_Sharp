79a09cf5d6d2fb88c45df352205eb078c163c63c
Ashraful Islam <102016365+Ashraful093@users.noreply.github.com>
2022-06-06 19:43:19 +0600

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+            long factorial = RecursiveFactorial(number);
+            Console.Write($"Factorial of {number} is: {factorial}");
+
+            Console.ReadLine();
+        }
+        static long RecursiveFactorial(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * RecursiveFactorial(number - 1);
+            }
+            
+
+        }
+    }
+}
---
+++ exception task.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptions_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int number1 = 4535;
+            int number2 = 0;
+            try
+            {
+                Console.WriteLine(number1 / number2);
+            }
+            catch (DivideByZeroException d)
+            {
+                Console.WriteLine(d.Message);  
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine(" the result is undefined");
+            }
+
+           
+            Console.WriteLine("Your age:");
+            string line = Console.ReadLine();
+            try
+            {
+                int age = Int32.Parse(line);
+            }
+            catch (FormatException f)
+            {
+                Console.WriteLine(f.Message);
+
+
+            }
+            finally
+            {
+                Console.WriteLine("{0} is not an integer , so it can't be your age", line);
+
+            }
+            bool flag = true;
+            try
+            {
+                IConvertible conv = flag;
+                Char ch = conv.ToChar(null);
+                Console.WriteLine("Conversion succeeded.");
+            }
+            catch (InvalidCastException ie)
+            {
+                Console.WriteLine(ie.Message);
+
+            }
+
+            try
+            {
+                Char ch = Convert.ToChar(flag);
+                Console.WriteLine("Conversion succeeded.");
+            }
+            catch (InvalidCastException ie)
+            {
+                Console.WriteLine(ie.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Cannot convert a Boolean to a Char.");
+                Console.WriteLine("Cannot convert a Boolean to a Char.");
+            }
+        }
+    }
+}
