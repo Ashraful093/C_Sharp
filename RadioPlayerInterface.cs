330631d9fe5a5eae6ed7ce916de6040c6037da30
Ashraful Islam <102016365+Ashraful093@users.noreply.github.com>
2022-06-07 23:11:42 +0600

    Add files via upload

---
+++ RadioPlayerInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_3
+{
+    public interface RadioPlayerInterface
+    {
+        void SwitcH(Boolean on);
+        void retune(double frequency);
+        void SetVolume(int Loudness);
+        void changeChannel();
+    }
+}
+
